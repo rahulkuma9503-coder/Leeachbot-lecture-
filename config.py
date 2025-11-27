72d07990ac7d3108bc75aa47e8c8f12599dd09f3
rahulkuma9503-coder <rahulkuma9503@gmail.com>
2025-11-27 22:30:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
+api_id = "23888050"
+api_hash = "80679c77353535f9e13f10710a7feec9"
+bot_token = "7691354451:AAHZl58tU7bX22Wd7MflGv-2s9-_b5VA778"
 auth_users = [7456681709]
 sudo_users = [7456681709]
